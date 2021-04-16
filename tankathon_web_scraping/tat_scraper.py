d55b3391522d2641dcb24c4191804004306ada55
statxsphere <agarwalsshubham0@gmail.com>
2021-04-17 02:23:05 +0530

    added functionality to get 'None' at suitable places

--- tankathon_web_scraping/tat_scraper.py
+++ tankathon_web_scraping/tat_scraper.py
@@ -1,15 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 
+years = list(range(2011,2019))
 file = requests.get("http://www.tankathon.com/past_drafts/2018").text
 soup = BeautifulSoup(file, 'lxml')
+l = []
 
 for match_adv in soup.find_all('div', class_='stats-advanced'):
     try:
         match_usg = match_adv.find('div', class_='stat reb')
         match1 = match_usg.find('div')
         match2 = match_usg.find('div', class_='label')
-        print(match1.text)
+        if match_usg:
+            l.append(match1.text)
+
     except:
-        continue
+        l.append(None)
+print(l)
 
