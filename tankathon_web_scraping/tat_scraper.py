21b3451770352e4d92b2dd74818f4dc2d17d2b2b
statxsphere <agarwalsshubham0@gmail.com>
2021-04-17 18:27:21 +0530

    resolved issue for player with no stats and converted each attribute to the right dtype

--- tankathon_web_scraping/tat_scraper.py
+++ tankathon_web_scraping/tat_scraper.py
@@ -39,85 +39,92 @@ def collect_singleyr_stats(year):
 
     #pick
     for picks in soup.find_all('div', class_='mock-row-pick-number'):
-        pick.append(picks.text)
+        pick.append(int(picks.text))
 
     # measurements
     for measurements in soup.find_all('div', class_='section height-weight'):
-        # print(measurements)
         a = measurements.text.split('"')
-        height.append(a[0])
-        weight.append(a[1])
+        heights = a[0].split("'")
+        heights1 = int(heights[0]) * 12 + float(heights[1])
+        height.append(round(heights1 * 2.54, 1))
+        weight.append(int(a[1][0:4]))
 
     #age
     for i in soup.find_all('div', class_='section year-age desktop'):
         x = i.find_all('div')
         c_year.append(x[0].text)
-        age.append(x[1].text)
+        age.append(float(x[1].text[0:4]))
 
     # per-36
-    for match_36 in soup.find_all('div', class_='stats-per-36'):
+    for stats in soup.find_all('div', class_='mock-row-stats'):
         try:
+            match_36 = stats.find('div', class_='stats-per-36')
             point = match_36.find('div', class_='stat pts')
             match1 = point.find('div')
-            points.append(match1.text)
+            points.append(float(match1.text))
         except:
             points.append(None)
 
         try:
+            match_36 = stats.find('div', class_='stats-per-36')
             reb = match_36.find('div', class_='stat reb')
             match2 = reb.find('div')
-            rebounds.append(match2.text)
+            rebounds.append(float(match2.text))
         except:
             rebounds.append(None)
 
         try:
+            match_36 = stats.find('div', class_='stats-per-36')
             ast = match_36.find('div', class_='stat ast')
             match3 = ast.find('div')
-            assists.append(match3.text)
+            assists.append(float(match3.text))
         except:
             assists.append(None)
 
         try:
+            match_36 = stats.find('div', class_='stats-per-36')
             stl = match_36.find('div', class_='stat stl desktop')
             match4 = stl.find('div')
-            steals.append(match4.text)
+            steals.append(float(match4.text))
         except:
             steals.append(None)
 
         try:
+            match_36 = stats.find('div', class_='stats-per-36')
             blk = match_36.find('div', class_='stat blk desktop')
             match5 = blk.find('div')
-            blocks.append(match5.text)
+            blocks.append(float(match5.text))
         except:
             blocks.append(None)
 
-    for match_adv in soup.find_all('div', class_='stats-advanced'):
         try:
+            match_adv = stats.find('div', class_='stats-advanced')
             TSp = match_adv.find('div', class_='stat pts')
             match6 = TSp.find('div')
-            TS.append(match6.text)
+            TS.append(float(match6.text))
         except:
             TS.append(None)
 
         try:
+            match_adv = stats.find('div', class_='stats-advanced')
             usage = match_adv.find('div', class_='stat reb')
             match7 = usage.find('div')
-            usg.append(match7.text)
+            usg.append(float(match7.text))
         except:
             usg.append(None)
 
         try:
+            match_adv = stats.find('div', class_='stats-advanced')
             bpo = match_adv.find('div', class_='stat ast')
             match8 = bpo.find('div')
-            obp.append(match8.text)
+            obp.append(float(match8.text))
         except:
             obp.append(None)
 
         try:
-            bpd = match_adv.find('div', class_='stat stl desktop')
+            match_adv = stats.find('div', class_='stats-advanced')
+            bpd = match_adv.find('div', class_='stat blk desktop')
             match9 = bpd.find('div')
-            dbp.append(match9.text)
+            dbp.append(float(match9.text))
         except:
             dbp.append(None)
-
-
